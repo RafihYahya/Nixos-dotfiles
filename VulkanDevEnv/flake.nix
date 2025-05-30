f9d93a84bf19e17069c09e2ffdfe51c199d3893b
rafihyahya <rafihyahya@gmail.com>
2025-05-31 01:40:42 +0200

    Added Vulkan Fixes

--- VulkanDevEnv/flake.nix
+++ VulkanDevEnv/flake.nix
@@ -40,21 +40,21 @@
                 vulkan-extension-layer
             ];
 
-            shellHook = with pkgs; '
-                echo "Welcome to my Vulkan Shell"
-                echo "vulkan loader: ${vulkan-loader}"
-                echo "vulkan headers: $vulkan-headers}"
-                echo "validation layer: ${vulkan-validation-layers}"
-                echo "tools: ${vulkan-tools}"
-                echo "tools-lunarg: ${vulkan-tools-lunarg}"
-                echo "extension-layer: ${vulkan-extension-layer}"
-            ';
+            shellHook = ''
+		  echo "Welcome to my Vulkan Shell"
+		  echo "vulkan loader: ${pkgs.vulkan-loader}"
+		  echo "vulkan headers: ${pkgs.vulkan-headers}"
+		  echo "validation layer: ${pkgs.vulkan-validation-layers}"
+		  echo "tools: ${pkgs.vulkan-tools}"
+		  echo "tools-lunarg: ${pkgs.vulkan-tools-lunarg}"
+		  echo "extension-layer: ${pkgs.vulkan-extension-layer}"
+		'';
 
-            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
-            VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
-            VULKAN_SDK = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
-            XDG_DATA_DIRS = builtins.getEnv "XDG_DATA_DIRS";
-            XDG_RUNTIME_DIR = "/run/user/1000";
+            #LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+            #VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+            #VULKAN_SDK = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+            #XDG_DATA_DIRS = builtins.getEnv "XDG_DATA_DIRS";
+            #XDG_RUNTIME_DIR = "/run/user/1000";
         };
     };
 }
