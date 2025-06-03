b8b4dab9f6744fb5d74fb7eeb3be68f95f14f432
rafihyahya <rafihyahya@gmail.com>
2025-06-03 20:11:45 +0200

    Adding Opengl DevShell

--- OpenGLDevEnv/flake.nix
+++ OpenGLDevEnv/flake.nix
@@ -13,7 +13,7 @@
         };
 	glad = pkgs.fetchurl {
           url = "https://github.com/RafihYahya/C-EXPLORATIONS/blob/Experimental/MODULES/OPENGL/glad.c";
-          sha256 = "";
+          sha256 = "sha256-E1FTR0CZuGg/2lnhBDOA/QWUHwzUMnkar+Lu68c5UTQ=";
         };
     in
         {
