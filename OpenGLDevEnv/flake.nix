e16e6ec5aa97d1f2e9f85f157a088389081c8ccb
rafihyahya <rafihyahya@gmail.com>
2025-06-03 20:03:55 +0200

    Adding Opengl DevShell

---
+++ OpenGLDevEnv/flake.nix
@@ -0,0 +1,50 @@
+{
+    description = "Vulkan Dev Flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }@inputs:
+    let
+        lib = nixpkgs.lib;
+        pkgs = import inputs.nixpkgs {
+            system = "x86_64-linux";
+        };
+	glad = pkgs.fetchurl {
+          url = "https://github.com/RafihYahya/C-EXPLORATIONS/blob/Experimental/MODULES/OPENGL/glad.c";
+          sha256 = "";
+        };
+    in
+        {
+        devShells.x86_64-linux.default = pkgs.mkShell rec {
+            name = "OpenGl Env";
+            nativeBuildInputs = with pkgs;[ gcc gdb wget ];
+
+            buildInputs = with pkgs;[
+                libxkbcommon
+                libGL
+		libGLU
+                glm
+                pkg-config
+                xorg.libX11
+                xorg.libXrandr
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXxf86vm
+                glfw glfw3
+                glslang
+            ];
+
+            shellHook = ''
+		   echo "Glad.c available at: ${myFile}"
+		'';
+
+            #LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+            #VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+            #VULKAN_SDK = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+            #XDG_DATA_DIRS = builtins.getEnv "XDG_DATA_DIRS";
+            #XDG_RUNTIME_DIR = "/run/user/1000";
+        };
+    };
+}
